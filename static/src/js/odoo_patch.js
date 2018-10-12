06d503bedff0645d738ec57d0bb6dfe24a3714da
Gilbert <gilbert@osbzr.com>
2018-10-12 17:56:50 +0800

    可以配置edit create delete等参数实现动态（根据每条记录）的显示删除，新建、编辑按钮

--- static/src/js/odoo_patch.js
+++ static/src/js/odoo_patch.js
@@ -1,27 +1,16 @@
-odoo.define('account.tour', function(require) {
+odoo.define('odoo.patch', function(require) {
 "use strict";
 
-var core = require('web.core');
-var tour = require('web_tour.tour');
+var FormController = require('web.FormController');
 
-var _t = core._t;
-
-tour.register('account_tour', {
-    'skip_enabled': true,
-}, [{
-    trigger: ".o_form_readonly .o_invoice_validate",
-    extra_trigger: '.o_invoice_form',
-    content: _t("Click to <b>validate your invoice.</b> A reference will be assigned to this invoice and you will not be able to modify it anymore."),
-    position: "right"
-}, {
-    trigger: ".o_invoice_send",
-    extra_trigger: '.o_invoice_form',
-    content: _t("Click to <b>send the invoice by email.</b>"),
-    position: "bottom"
-}, {
-    trigger: ".o_mail_send",
-    content: _t("Click to <b>send the invoice.</b>"),
-    position: "bottom"
-} ]);
+ FormController.include({
+    is_action_enabled: function (action) {
+        if (['true', 'false', '0', '1'].indexOf(this.activeActions[action]) < 0 && this.activeActions[action].domain){
+            return JSON.parse(py.evaluate(py.parse(py.tokenize(this.activeActions[action].domain)), this.initialState.data).toJSON());
+        }else {
+             return this.activeActions[action]
+        }
+    },
+ });
 
 });
