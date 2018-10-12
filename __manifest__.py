360b66ae025f56fe8a7c7616d6c81c255f7b7988
Gilbert <gilbert@osbzr.com>
2018-10-12 18:16:30 +0800

    添加作者

--- __manifest__.py
+++ __manifest__.py
@@ -3,6 +3,7 @@
 {
     'name' : 'odoo_patch',
     'version' : '1.1',
+    'author': 'gilbert(静静gilbert@osbzr.com)',
     'summary': """
     可以配置edit create delete等参数实现动态（根据每条记录）的显示删除，新建、编辑按钮
         edit='{"color_domain": "state!=\"draft\""}'
